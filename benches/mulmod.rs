1131e003d7f84efa35a06b571d27911afdc47cdc
Josh Stone <cuviper@gmail.com>
2016-03-10 17:09:15 -0800

    Benchmark mpz and bigint modular multiplication

---
+++ benches/mulmod.rs
@@ -0,0 +1,29 @@
+#![feature(test)]
+
+extern crate num;
+extern crate gmp;
+extern crate test;
+
+use gmp::mpz::Mpz;
+use num::{Num, BigInt};
+use test::Bencher;
+
+const P: &'static str = "13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084171";
+const G: &'static str = "11717829880366207009516117596335367088558084999998952205599979459063929499736583746670572176471460312928594829675428279466566527115212748467589894601965568";
+const H: &'static str = "3239475104050450443565264378728065788649097520952449527834792452971981976143292558073856937958553180532878928001494706097394108577585732452307673444020333";
+
+#[bench]
+fn mpz_mulmod(b: &mut Bencher) {
+    let p = Mpz::from_str_radix(P, 10).unwrap();
+    let g = Mpz::from_str_radix(G, 10).unwrap();
+    let h = Mpz::from_str_radix(H, 10).unwrap();
+    b.iter(|| (&g * &h) % &p);
+}
+
+#[bench]
+fn bigint_mulmod(b: &mut Bencher) {
+    let p = BigInt::from_str_radix(P, 10).unwrap();
+    let g = BigInt::from_str_radix(G, 10).unwrap();
+    let h = BigInt::from_str_radix(H, 10).unwrap();
+    b.iter(|| (&g * &h) % &p);
+}
